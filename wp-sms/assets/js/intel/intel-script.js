34c886fd65bbcf0a4d5828a5f23aa46cb2c34d54
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 02:33:07 +0000

    6.9.5

---
+++ wp-sms/assets/js/intel/intel-script.js
@@ -0,0 +1,133 @@
+jQuery(document).ready(function () {
+
+    setTimeout(init, 1500);
+});
+
+
+function init() {
+    const body = document.body;
+    const direction = body.classList.contains('rtl') ? 'rtl' : 'ltr';
+
+    const { only_countries, preferred_countries } = wp_sms_intel_tel_input;
+    let defaultCountry;
+
+    if (only_countries.length > 0) {
+        if (preferred_countries.length > 0 && preferred_countries.every(country => only_countries.includes(country))) {
+            defaultCountry = preferred_countries[0];
+        } else {
+            defaultCountry = only_countries[0];
+        }
+    } else {
+        defaultCountry = preferred_countries.length > 0 ? preferred_countries[0] : 'us';
+    }
+
+     // Initialize input fields with intlTelInput
+
+    const useFullscreenPopupOption =  typeof navigator !== "undefined" && typeof window !== "undefined" ? (
+        //* We cannot just test screen size as some smartphones/website meta tags will report desktop resolutions.
+        //* Note: to target Android Mobiles (and not Tablets), we must find 'Android' and 'Mobile'
+        /Android.+Mobile|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+        ) || window.innerWidth <= 500
+    ) : false;
+
+    function initializeInputs(inputTells) {
+        for (var i = 0; i < inputTells.length; i++) {
+              if (inputTells[i] && inputTells[i].nodeName === 'INPUT') {
+                 inputTells[i].setAttribute('dir', direction);
+                  let iti = window.intlTelInput(inputTells[i], {
+                    separateDialCode: false,
+                    allowDropdown: true,
+                    strictMode: true,
+                    onlyCountries: wp_sms_intel_tel_input.only_countries,
+                    countryOrder: wp_sms_intel_tel_input.preferred_countries,
+                     nationalMode: true,
+                    useFullscreenPopup: useFullscreenPopupOption,
+                    dropdownContainer: body.classList.contains('rtl') ? null : body,
+                    utilsScript: wp_sms_intel_tel_input.util_js,
+                    hiddenInput: () => ({ phone: inputTells[i].name}),
+                    formatOnDisplay: false,
+                    initialCountry: defaultCountry
+                  });
+                  function setDefaultCode(item){
+                       if(item.value==''){
+                          let country=iti.getSelectedCountryData();
+                          item.value = '+'+country.dialCode;
+                      }else{
+                          if(iti.getNumber()){
+                               item.value=iti.getNumber().replace(/[-\s]/g, '')
+                          }else{
+                              item.value=item.value.replace(/[-\s]/g, '')
+                          }
+                      }
+                  }
+                  setDefaultCode(inputTells[i]);
+                  inputTells[i].addEventListener('blur', function() {
+                      setDefaultCode(this)
+                  });
+            }
+
+         }
+    }
+
+    // Check and initialize the main input fields
+    function checkAndInitializeInputs() {
+        const primaryInput = document.querySelectorAll('#billing-wpsms\\/mobile');
+        const isWooCommerceCheckoubBlock = document.querySelector('.wc-block-checkout ');
+        if (isWooCommerceCheckoubBlock) {
+            return null;
+        }
+        if (!primaryInput.length) {
+            inputTells = document.querySelectorAll(".wp-sms-input-mobile, .wp-sms-input-mobile #billing_phone,#billing-phone , #wp-sms-input-mobile, .user-mobile-wrap #mobile");
+        }
+        initializeInputs(inputTells);
+    }
+
+    checkAndInitializeInputs();
+
+    // Additional specific input field initialization
+    var inputTell = document.querySelector("#job_mobile, #_job_mobile");
+    if (inputTell && !inputTell.getAttribute('placeholder')) {
+        inputTell.setAttribute('dir', direction)
+        let iti_job = window.intlTelInput(inputTell, {
+            autoInsertDialCode: true,
+            autoPlaceholder: "aggressive",
+            allowDropdown: true,
+            strictMode: true,
+            useFullscreenPopup: false,
+            dropdownContainer: body.classList.contains('rtl') ? null : body,
+            onlyCountries: wp_sms_intel_tel_input.only_countries,
+            countryOrder: wp_sms_intel_tel_input.preferred_countries,
+            autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
+            nationalMode: true,
+            utilsScript: wp_sms_intel_tel_input.util_js,
+            formatOnDisplay: false,
+            initialCountry: defaultCountry
+         });
+        function setDefaultCode(item){
+             if(item.value==''){
+                let country=iti_job.getSelectedCountryData();
+                item.value = '+'+country.dialCode;
+            }else{
+                if(iti_job.getNumber()){
+                     item.value=iti_job.getNumber().replace(/[-\s]/g, '')
+                }else{
+                    item.value=item.value.replace(/[-\s]/g, '')
+                }
+            }
+        }
+        inputTell.addEventListener('blur', function() {
+            setDefaultCode(this)
+        });
+    }
+
+    // Handle the change event for the checkbox
+    const shippingCheckbox = document.querySelector('#shipping-fields .wc-block-checkout__use-address-for-billing input');
+    if (shippingCheckbox) {
+        shippingCheckbox.addEventListener('change', function() {
+            if (document.querySelector('#billing-fields')) {
+                setTimeout(checkAndInitializeInputs, 500);
+            }
+        });
+    }
+}
---
+++ wp-sms/wp-sms.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Plugin Name: WP SMS
+ * Plugin URI: https://wp-sms-pro.com/
+ * Description: Ultimate SMS & MMS Notifications, 2FA, OTP, and Integrations with WooCommerce, GravityForms, and More
+ * Version: 6.9.5
+ * Author: VeronaLabs
+ * Author URI: https://veronalabs.com/
+ * Text Domain: wp-sms
+ * Domain Path: /languages
+ * GitHub Plugin URI: https://github.com/veronalabs/wp-sms
+ * License: GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 4.1
+ * Requires PHP: 5.6
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+/**
+ * Load Plugin Defines
+ */
+include_once __DIR__ . '/includes/defines.php';
+
+/**
+ * Load plugin Special Functions
+ */
+require_once WP_SMS_DIR . 'includes/functions.php';
+
+/**
+ * Initial gateway
+ */
+require_once WP_SMS_DIR . 'includes/class-wpsms-gateway.php';
+
+$sms = wp_sms_initial_gateway();
+
+/**
+ * Load Plugin
+ */
+require WP_SMS_DIR . 'includes/class-wpsms.php';
+
+/**
+ * @return object|WP_SMS|null
+ */
+function WPSms()
+{
+    return WP_SMS::get_instance();
+}
+
+WPSms();
